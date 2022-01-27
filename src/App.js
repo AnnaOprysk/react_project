92e58f8674800a8af000c8e209d108b6178c6c3e
AnnaOprysk <ani4ka88@yahoo.com>
2022-01-27 18:23:06 +0200

    Branch-Homework4

--- src/App.js
+++ src/App.js
@@ -3,26 +3,52 @@ import {useReducer} from "react";
 
 const reducer = (state, action) => {
     switch (action.type) {
-        case 'inc':
-            return {...state, count: state.count + 1};
-        case 'dec':
-            return {...state, count: state.count - 1};
-        case 'reset':
-            return {...state, count: action.payload = 0};
+        case 'inc1':
+            return {...state, count1: state.count1 + 1};
+        case 'dec1':
+            return {...state, count1: state.count1 - 1};
+        case 'reset1':
+            return {...state, count1: action.payload = 0};
+        case 'inc2':
+            return {...state, count2: state.count2 + 1};
+        case 'dec2':
+            return {...state, count2: state.count2 - 1};
+        case 'reset2':
+            return {...state, count2: action.payload = 0};
+        case 'inc3':
+            return {...state, count3: state.count3 + 1};
+        case 'dec3':
+            return {...state, count3: state.count3 - 1};
+        case 'reset3':
+            return {...state, count3: action.payload = 0};
         default:
             return state;
     }
 }
 
 function App() {
-    const [state, dispatch] = useReducer(reducer,{count: 0});
+    const [state, dispatch] = useReducer(reducer,{count1: 0,count2:0,count3:0});
 
     return (
         < div>
-            <div className={'count'}> {state.count}</div>
-            <button onClick={() => dispatch({type: 'inc'})}>Increment</button>
-            <button onClick={() => dispatch({type: 'dec'})}>Decrement</button>
-            <button onClick={() => dispatch({type: 'reset', payload: 0})}>Reset</button>
+            <div>
+                <div className={'count'}>1). {state.count1}</div>
+                <button onClick={() => dispatch({type: 'inc1'})}>Increment</button>
+                <button onClick={() => dispatch({type: 'dec1'})}>Decrement</button>
+                <button onClick={() => dispatch({type: 'reset1', payload: 0})}>Reset</button>
+            </div>
+            <div>
+                <div className={'count'}>2). {state.count2}</div>
+                <button onClick={() => dispatch({type: 'inc2'})}>Increment</button>
+                <button onClick={() => dispatch({type: 'dec2'})}>Decrement</button>
+                <button onClick={() => dispatch({type: 'reset2', payload: 0})}>Reset</button>
+            </div>
+            <div>
+                <div className={'count'}>3). {state.count3}</div>
+                <button onClick={() => dispatch({type: 'inc3'})}>Increment</button>
+                <button onClick={() => dispatch({type: 'dec3'})}>Decrement</button>
+                <button onClick={() => dispatch({type: 'reset3', payload: 0})}>Reset</button>
+            </div>
         < /div>
     );
 }
