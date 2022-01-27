e8d5a8697b9a8e187d45f529e82821e5f15288b1
AnnaOprysk <ani4ka88@yahoo.com>
2022-01-27 13:11:51 +0200

    Branch-Homework4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'inc':
+            return {...state, count: state.count + 1};
+        case 'dec':
+            return {...state, count: state.count - 1};
+        case 'reset':
+            return {...state, count: action.payload = 0};
+        default:
+            return state;
+    }
+}
 
 function App() {
-  return (
-    <div>
+    const [state, dispatch] = useReducer(reducer,{count: 0});
 
-    </div>
-  );
+    return (
+        < div>
+            <div className={'count'}> {state.count}</div>
+            <button onClick={() => dispatch({type: 'inc'})}>Increment</button>
+            <button onClick={() => dispatch({type: 'dec'})}>Decrement</button>
+            <button onClick={() => dispatch({type: 'reset', payload: 0})}>Reset</button>
+        < /div>
+    );
 }
 
 export default App;
