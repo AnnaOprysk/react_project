9a7d4fabeabdec4fbc0117be810993aadde85a99
AnnaOprysk <ani4ka88@yahoo.com>
2022-01-31 15:06:28 +0200

    Branch-Classwork4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 1:
+            return{...state, }
+        default:
+            return state;
+    }
+}
 
 function App() {
-  return (
-    <div>
+    const [state, dispatch] = useReducer(reducer, {cat: '',dog:''});
+    console.log(state.name)
 
-    </div>
-  );
+
+    return (
+        <div>
+            <form>
+                <label> Add Cat:
+                    <input type="text" name={'cat'} onChange={reducer}/>
+                    <button onClick={() => dispatch({type: 'cat'})}>Save</button>
+                </label>
+                <label> Add dog:
+                    <input type="text" name={'dog'} onChange={reducer}/>
+                    <button onClick={() => dispatch({type: 'dog'})}> Save</button>
+                </label>
+
+            </form>
+            <hr/>
+            <div>
+                {state.value}
+                <button>Delete</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
