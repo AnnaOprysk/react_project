2033535b7682fb1988eb69c97c57c2e3e969edcf
AnnaOprysk <ani4ka88@yahoo.com>
2022-02-03 18:18:42 +0200

    Branch-Classwork5_myVersion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
+import {useState} from "react";
+
 import './App.css';
+import css from './components/ListItem/Checkbox.module.css';
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+
 
 function App() {
-  return (
-    <div>
+    const [text, setText] = useState('');
+    const [todoList, setTodoList] = useState([]);
+    const [checkbox, setCheckbox] = useState({});
 
-    </div>
-  );
+    function addTodoList() {
+        setTodoList([...todoList, {id: new Date().getTime(), text, status:false}]);
+        setText('');
+    }
+
+    return (
+        <div>
+            <div className={'inputBlock'}>
+                <input className={'inputMain'} type="text" placeholder={'Enter your to do list...'} value={text}
+                       onChange={e => setText(e.target.value)}/>
+                <button className={'inputBtn'} onClick={() => addTodoList(text)}>Save</button>
+            </div>
+            <hr/>
+            <TransitionGroup component='ul'>
+                {todoList.map(({id, text}) =>
+                    <CSSTransition key={id} timeout={500} classNames='todo'>
+                        <h2 className={'todo'}>
+
+                            <input type="checkbox" onClick={() => setCheckbox(!checkbox)}/>{checkbox? {text} : <s>{text}</s> }
+
+                            {/*<div> {status===false ? {text} : <s>{text}</s> }</div>*/}
+                            {/*<div className={checkbox===true? css.checkbox : ''}>   {text} </div>*/}
+
+                            <button
+                                onClick={() => setTodoList([...todoList.filter(value => value.id !== id)])}>Delete
+                            </button>
+                        </h2>
+                    </CSSTransition>
+
+                )}
+            </TransitionGroup>
+        </div>
+    );
 }
 
 export default App;
